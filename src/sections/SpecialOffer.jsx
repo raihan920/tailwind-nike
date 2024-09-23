1ee5b3d7ff5bd723f81c05e82d6f1b614f78ab34
Raihan Chowdhury <raihan92@rocketmail.com>
2024-09-23 21:45:41 +0600

    Special offer section done

--- src/sections/SpecialOffer.jsx
+++ src/sections/SpecialOffer.jsx
@@ -1,6 +1,29 @@
+import { arrowRight } from "../assets/icons";
+import { offer } from "../assets/images";
+import Button from "../components/Button";
+
 const SpecialOffer = () => {
   return (
-    <div>SpecialOffer</div>
+    <section className="flex justify-wrap items-center max-xl:flex-col-reverse gap-10 max-container">
+      <div className="flex-1">
+        <img src={offer} width={773} height={687} className="object-contain w-full"/>
+      </div>
+      <div className="flex flex-1 flex-col">        
+          <h2 className="font-palanquin text-4xl capitalize font-bold lg:max-w-lg">
+            <span className="text-coral-red"> Special</span> Offer
+          </h2>
+          <p className="mt-4 lg:max-w-lg info-text">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa cumque sit ea necessitatibus perspiciatis porro saepe at ullam. Quis quidem nostrum doloribus quos possimus quia optio beatae architecto obcaecati id!
+          </p>
+          <p className="mt-6 lg:max-w-lg info-text">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum voluptatem consectetur ut? Animi aliquid aperiam iusto accusamus fugit earum provident.
+          </p>
+          <div className="mt-11 flex flex-wrap gap-4">
+            <Button label="Shop now" iconURL={arrowRight} />
+            <Button label="Learn more" backgroundColor="bg-white" borderColor="border-slate-gray" text-color="text-slate-gray" />
+          </div>
+      </div>
+    </section>
   )
 }
 
